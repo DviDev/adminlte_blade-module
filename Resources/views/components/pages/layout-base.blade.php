ca603eb836627d7c54bc8f2f9e1d55b60f029f6f
Davi Menezes <davimenezes.dev@gmail.com>
2023-08-07 16:26:38 -0300

    ui pages

--- Resources/views/components/pages/layout-base.blade.php
+++ Resources/views/components/pages/layout-base.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | ChartJS</title>
+    <title>{{config('app.name')}} | {{$title}}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -27,7 +27,7 @@
                 <a href="{{route('home')}}" class="nav-link">Home</a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Contact</a>
+                <a href="#" class="nav-link">{{$breadcrumb}}</a>
             </li>
         </ul>
 
