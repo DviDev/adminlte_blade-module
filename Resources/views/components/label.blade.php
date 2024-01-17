dacab25f04c0eff7a7759f550d55641617c40545
Davi Menezes <davimenezes.dev@gmail.com>
2024-01-17 19:24:31 -0300

    improvements refactorings

--- Resources/views/components/label.blade.php
+++ Resources/views/components/label.blade.php
@@ -1,6 +1,6 @@
 @props([
-    'for',
-    'value',
+    'for' => null,
+    'value' => '',
     'required' => false
 ])
 <label {{ $attributes->class([
@@ -8,7 +8,9 @@
     'font-medium' => $required,
     'text-gray-700' => !$errors->has($for),
     'text-danger' => $errors->has($for),
-    ])}} for="{{$for}}" style="border-bottom: none !important; margin-bottom: 0 !important;"
+    ])}}
+       for="{{$for}}"
+       style="border-bottom: none !important; margin-bottom: 0 !important;"
     @if($errors->has($for))
         title="{{collect($errors->get($for))->first()}}"
     @endif
--- View/Components/Form/Summernote.php
+++ View/Components/Form/Summernote.php
@@ -6,18 +6,6 @@ use Modules\DvUi\View\BaseBladeComponent;
 
 class Summernote extends BaseBladeComponent
 {
-    public function __construct(
-        public array|null      $attr = null,
-        public string|int|null $id = null,
-        public string|null     $label = null,
-        public bool            $required = false,
-        public bool            $validate = false,
-        public             $value = null,
-    )
-    {
-        parent::__construct($attr, $label, $required, $validate, $value, $id);
-    }
-
     public function render()
     {
         return view('lte::components.form.summernote');
