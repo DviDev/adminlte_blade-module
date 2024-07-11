a05763e9ecdbe3bd1adb192cda9c1835cfcb9c74
Davi Menezes <davimenezes.dev@gmail.com>
2024-07-10 22:59:45 -0300

    layout header

--- Resources/views/components/pages/config/config_list_page.blade.php
+++ Resources/views/components/pages/config/config_list_page.blade.php
@@ -1,11 +1,3 @@
 <x-lte::layout.v1.page>
-    <x-slot:header>
-        <span class="card-title">Configurações</span>
-        <div class="card-tools">
-            <a href="{{route('admin.config')}}" class="btn btn-primary btn-sm">
-                <i class="fas fa-plus"></i> Cadastrar
-            </a>
-        </div>
-    </x-slot:header>
     <livewire:app::config.config-list/>
 </x-lte::layout.v1.page>
