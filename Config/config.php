02c99dfcd9e508f6d3176e6dc82c1e21976eb124
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-17 04:05:26 -0300

    refactorings

--- Config/config.php
+++ Config/config.php
@@ -4,11 +4,11 @@ return [
     'name' => 'Lte',
     'user' => [
         'profile' => [
-            'activity' => false,
-            'timeline' => false,
+            'activity' => env('MODULE_LTE_USER_PROFILE_ACTIVITY', false),
+            'timeline' => env('MODULE_LTE_USER_PROFILE_TIMELINE', false),
         ]
     ],
     'theme' => [
-        'dark_mode' => false,
+        'dark_mode' => env('MODULE_LTE_THEME_DARK_MODE', false),
     ],
 ];
