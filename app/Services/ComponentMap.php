82508b5670ad73d722f5f1799dbfc51461e3dafb
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-01 16:09:41 -0300

    wip

--- app/Services/ComponentMap.php
+++ app/Services/ComponentMap.php
@@ -4,7 +4,9 @@ namespace Modules\Lte\Services;
 
 use Modules\DvUi\Enums\DvuiComponentAlias;
 use Modules\DvUi\Interfaces\DvuiComponentSuiteContract;
+use Modules\Lte\View\Components\Form\Date;
 use Modules\Lte\View\Components\Form\Input;
+use Modules\Lte\View\Components\Form\Select;
 use Modules\Lte\View\Components\Layout\Page;
 
 class ComponentMap implements DvuiComponentSuiteContract
@@ -19,6 +21,8 @@ class ComponentMap implements DvuiComponentSuiteContract
         return [
             DvuiComponentAlias::LayoutAdminPage->value => Page::class,
             DvuiComponentAlias::FormInput->value => Input::class,
+            DvuiComponentAlias::FormSelect->value => Select::class,
+            DvuiComponentAlias::FormDate->value => Date::class,
         ];
     }
 }
