1ff4a3fb3ab00e85c9cf81777f0c2ab9df31c055
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-01 16:10:36 -0300

    wip

--- resources/views/components/card/body.blade.php
+++ resources/views/components/card/body.blade.php
@@ -1,4 +1,4 @@
-<div {{$attributes->class(['card-body flex flex-col'])}}>
+<div {{$attributes->class(['card-body'])}}>
     <x-lte::dev.info :alias="$componentAlias->value"/>
     @if(isset($tabs))
         <div {{$tabs->attributes->class(["tab-content"])}} id="custom-tabs-two-tabContent">
