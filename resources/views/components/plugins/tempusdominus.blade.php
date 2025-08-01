c19d1d82b7df3263112b7c6f3c6a50c2b8d79eff
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-01 17:02:19 -0300

    wip

--- resources/views/components/plugins/tempusdominus.blade.php
+++ resources/views/components/plugins/tempusdominus.blade.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet"
           href="{{asset("assets/modules/lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css")}}">
 @endpushonce
+<x-lte::plugins.moment/>
 @pushonce('tempusdominus-bootstrap-4_js')
     <script
         src="{{asset("assets/modules/lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")}}"></script>
