4058df86074d16425e386a65bb80d0ea194a4a7f
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-07 23:19:39 -0300

    refactor input component to simplify attribute handling and replace trans() with __()

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -8,13 +8,13 @@
     if (count($attr) > 0) {
         $array = collect($attr)->except(['id'])->merge($attributes->getAttributes())->all();
         if (isset($array['name'])){
-            $array['name'] = trans($array['name']);
+            $array['name'] = __($array['name']);
         }
         if (isset($array['placeholder'])){
-            $array['placeholder'] = trans($array['placeholder']);
+            $array['placeholder'] = __($array['placeholder']);
         }
         if (isset($array['label'])){
-            $array['label'] = ucfirst(trans(strtolower($array['label'])));
+            $array['label'] = ucfirst(__(strtolower($array['label'])));
         }
         $attributes->setAttributes($array);
     }
@@ -26,7 +26,6 @@
     <x-lte::dev.info :alias="$componentAlias->value ?? 'lte.form.input'"/>
     <x-lte::input
         :txt="$txt ?? $label"
-        :label="$label"
-        :placeholder="$placeholder"
-        {{$attributes}}/>
+        {{$attributes->filter(fn($value, $key) => !in_array($key ,['value', 'id', 'name']))}}
+    />
 </div>
