5e3f08fed57567661e57cdd44c3131b35065510a
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-01 18:00:18 -0300

    Refactor: Simplify input form component and integrate BaseFormBladeComponent preparation
    Simplified attribute handling in the input form component by removing redundant localization logic and delegating preparation to `BaseFormBladeComponent::prepare`.

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,31 +1,19 @@
+@php use Modules\Base\View\Components\Form\BaseFormBladeComponent; @endphp
 @props([
-    'txt' => null,
-    'label' => null,
-    'placeholder' => null,
     'attr' => []
 ])
 @php
-    if (count($attr) > 0) {
-        $array = collect($attr)->except(['id'])->merge($attributes->getAttributes())->all();
-        if (isset($array['name'])){
-            $array['name'] = __($array['name']);
-        }
-        if (isset($array['placeholder'])){
-            $array['placeholder'] = __($array['placeholder']);
-        }
-        if (isset($array['label'])){
-            $array['label'] = ucfirst(__(strtolower($array['label'])));
-        }
-        $attributes->setAttributes($array);
-    }
+    $array = collect($attr)->except('id')->merge($attributes->getAttributes())->all();
+    $attributes->setAttributes($array);
+    BaseFormBladeComponent::prepare($attributes);
 @endphp
 <div @class([
         'w-full',
-        "form-group" => $label
+        "form-group" => $attributes->get('label')
         ])>
     <x-lte::dev.info :alias="$componentAlias->value ?? 'lte.form.input'"/>
     <x-lte::input
-        :txt="$txt ?? $label"
+        :txt="$attributes->get('label')"
         {{$attributes->filter(fn($value, $key) => !in_array($key ,['value', 'id', 'name']))}}
     />
 </div>
