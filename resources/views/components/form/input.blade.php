5ad264b9c38260ae607ce080187fe8bf8997e4c0
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-26 17:18:17 -0300

    Refactor to use `BaseFormBladeComponent` from Contracts namespace.
    Updated all references to `BaseFormBladeComponent` to reflect its move from the `View\Components\Form` namespace to `Contracts`. This change simplifies the structure and aligns with the new contracts architecture.

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
-@php use Modules\Base\View\Components\Form\BaseFormBladeComponent; @endphp
+@php use Modules\Base\Contracts\BaseFormBladeComponent; @endphp
 @props([
     'attr' => []
 ])
@@ -18,9 +18,9 @@
             <div>{{$attributes->get('value')}}</div>
         </div>
     @else
-    <x-lte::input
-        :txt="$attributes->get('label')"
-        {{$attributes->filter(fn($value, $key) => !in_array($key ,['value', 'id', 'name']))}}
-    />
+        <x-lte::input
+            :txt="$attributes->get('label')"
+            {{$attributes->filter(fn($value, $key) => !in_array($key ,['value', 'id', 'name']))}}
+        />
     @endif
 </div>
